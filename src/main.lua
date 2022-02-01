96478d12fcf46356d66cf3a32a69fee36332d21b
admodev <adolfomoyano@exegesisba.com>
2022-02-01 10:53:08 -0300

    add shooting function

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,16 @@
 player = { x = 350, y = 500,
 speed = 150, img = nil }
 
+canShoot = true
+delay = 0.2
+shootDelay = delay
+
+projectileImg = nil
+projectile = {}
+
 function love.load(arg)
     player.img = love.graphics.newImage('assets/ship.png')
+    projectileImg = love.graphics.newImage('assets/projectile.png')
 end
 
 function love.update(dt)
@@ -18,8 +26,33 @@ function love.update(dt)
     elseif love.keyboard.isDown('down') then
         player.y = player.y + (player.speed *dt)
     end
+
+    -- Shoot delay timer
+    shootDelay = shootDelay - (1*dt)
+    if shootDelay < 0 then
+        canShoot = true
+    end
+
+    if love.keyboard.isDown('space') and canShoot then
+        newProjectile = { x = player.x + (player.img:getWidth()/2), y = player.y, img = projectileImg  }
+        table.insert(projectile, newProjectile)
+        canShoot = false
+        shootDelay = delay
+    end
+
+    for i, bullet in ipairs(projectile) do
+        bullet.y = bullet.y - (250 *dt)
+
+        if bullet.y < 0 then
+            table.remove(projectile, i)
+        end
+    end
 end
 
 function love.draw(dt)
     love.graphics.draw(player.img, player.x, player. y)
+
+    for i, bullet in ipairs(projectile) do
+        love.graphics.draw(bullet.img, bullet.x, bullet.y)
+    end
 end
